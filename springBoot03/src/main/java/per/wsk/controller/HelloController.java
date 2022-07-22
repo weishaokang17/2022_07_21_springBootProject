4b8931791804b264ba4d59c192e1792e52f5f339
weishaokang <weishaokang17@163.com>
2022-07-22 18:05:56 +0800

    springBoot学习

---
+++ springBoot03/src/main/java/per/wsk/controller/HelloController.java
@@ -0,0 +1,14 @@
+package per.wsk.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/bug.jpg")
+    public String hello(){
+        return "aaaa";
+    }
+
+}
