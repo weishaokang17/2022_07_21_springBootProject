af92f7d143b7d9eb13e6ea7bba0f339d64f0f614
weishaokang <weishaokang17@163.com>
2022-07-23 00:15:47 +0800

    springBoot学习

--- springBoot03/src/main/java/per/wsk/controller/HelloController.java
+++ springBoot03/src/main/java/per/wsk/controller/HelloController.java
@@ -1,7 +1,6 @@
 package per.wsk.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -10,5 +9,33 @@ public class HelloController {
     public String hello(){
         return "aaaa";
     }
+
+
+    //    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    @GetMapping("/user")
+    public String getUser(){
+        return "GET-张三";
+    }
+
+    //    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    @PostMapping("/user")
+    public String saveUser(){
+        return "POST-张三";
+    }
+
+
+    //    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    @PutMapping("/user")
+    public String putUser(){
+        return "PUT-张三";
+    }
+
+    @DeleteMapping("/user")
+//    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "DELETE-张三";
+    }
+
+    //扩展点：如何把 _method  这个名字换成我们自己喜欢的
 
 }
